f756fe99d03152a0d1ca2799bb928007a457e7c7
Tech-Nick <nicholaspcosta@gmail.com>
2021-07-24 15:18:48 +0100

    updated the output of the dummy data to match the test spec

--- suade_test_proj/metrics_report/views.py
+++ suade_test_proj/metrics_report/views.py
@@ -1,29 +1,25 @@
-from django.shortcuts import render 
+from django.shortcuts import render
+import json 
 
 
-test_data = [
-	{
-		'firstname' : 'Nick',
-		'secondname' : 'Costa',
-		'title' : 'Mr',
-		'ammount' : '2000',
-		'total' : '2352',
-	},
-	{
-		'firstname' : 'Sarah',
-		'secondname' : 'Tomms',
-		'title' : 'Mrs',
-		'ammount' : '1389',
-		'total' : '67',
-	}
-]
+test_data = {
+				'firstname' : 'Nick',
+				'secondname' : 'Costa',
+				'title' : 'Mr',
+				'ammount' : '2000',
+				'total' : '2352'
+			}
+
+json_object = json.dumps(test_data, indent = 4)  
+
+
 
 def home(request):
 	return render(request, 'metrics_report/home.html')
 
 def report(request):
 	context = {
-		'posts': test_data,
+		'json': json_object,
 		'title': 'Metrics Report Data'
 
 	}
