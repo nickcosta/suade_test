cf9d635986e197bc07eae858f25cc0104945fee0
Tech-Nick <nicholaspcosta@gmail.com>
2021-07-24 17:44:53 +0100

    added dunder methods

--- suade_test_proj/metrics_report/admin.py
+++ suade_test_proj/metrics_report/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Order, Product, Promotion, ProductPromotion, VendorCommisions, OrderLine
 
-# Register your models here.
+admin.site.register(Order)
--- suade_test_proj/metrics_report/models.py
+++ suade_test_proj/metrics_report/models.py
@@ -1,26 +1,41 @@
 from django.db import models
 
 class Order(models.Model):
-	created_at = models.DateTimeField(auto_now_add=True)
+	created_at = models.DateTimeField()
 	vendor_id = models.IntegerField()
 	customer_id = models.IntegerField()
 
+	def __str__(self):
+		return f'{self.created_at} {self.vendor_id} {self.customer_id}'
+		
 
 class Product(models.Model):
 	description = models.CharField(max_length=255)
 
+	def __str__(self):
+		return f'{self.description}'
+
 class Promotion(models.Model):
 	description = models.CharField(max_length=255)
+
+	def __str__(self):
+		return f'{self.description}'
 
 class ProductPromotion(models.Model):
 	date = models.DateTimeField()
 	product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 	promotion_id = models.ForeignKey(Promotion, on_delete=models.CASCADE)
 
+	def __str__(self):
+		return f'{self.date} {self.product_id} {self.promotion_id}'
+
 class VendorCommisions(models.Model):
 	date = models.DateTimeField()
 	vendor_id = models.IntegerField()
 	rate = models.DecimalField(max_digits=8, decimal_places=2)
+
+	def __str__(self):
+		return f'{self.date} {self.vendor_id} {self.rate}'
 
 class OrderLine(models.Model):
 	order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
@@ -34,3 +49,7 @@ class OrderLine(models.Model):
 	dicounted_amount = models.DecimalField(max_digits=8, decimal_places=2)
 	vat_amount = models.IntegerField()
 	total_amount = models.IntegerField()
+
+	def __str__(self):
+		return f'''{self.order_id} {self.product_id} {self.product_description} {self.product_price} {self.product_vat_rate} {self.discount_rate}'''
+		f'''{self.quantity} {self.full_price_amount} {self.dicounted_amount} {self.vat_amount} {self.total_amount}'''
